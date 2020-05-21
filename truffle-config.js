153d0de6bc91620f214790664878d92281615cc0
meows <b5c6@protonmail.com>
2020-05-21 07:59:49 -0500

    sav
    Signed-off-by: meows <b5c6@protonmail.com>

--- truffle-config.js
+++ truffle-config.js
@@ -6,7 +6,9 @@ const fs = require('fs');
 const keyth = require('keythereum');
 
 const keyPubAddress = '7d38c531e7544625a4ea96437d7fe5c2918f3307';
-const keyFilePath = '/home/ia/dev/bakaoh/WrappedETC/keystore/key';
+const keyFilePath = '/home/ia/dev/bakaoh/WrappedETC/keystore/key-7d38c531e7544625a4ea96437d7fe5c2918f3307'; // unused.
+
+// Keythereum requires the key file to have to have its pub address in the file name
 const keyFileDir = '/home/ia/dev/bakaoh/WrappedETC/';
 
 const keyobj = keyth.importFromFile(keyPubAddress, keyFileDir);
@@ -24,9 +26,16 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
 
   networks: {
+    og: {
+      host: "127.0.0.1",
+      gas: 6283185,
+      port: 7545,
+      network_id: "*",
+    },
+
     development: {
       host: "127.0.0.1",
-      gas: 6350000,
+      gas: 6283185,
       port: 7545,
       network_id: "*",
     }
@@ -44,6 +53,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
+      version: "0.5.5",
       settings: {
        optimizer: {
          enabled: true,
