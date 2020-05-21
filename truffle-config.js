f358cc9eef525c9750837c7333b3d7074ed09d1d
meows <b5c6@protonmail.com>
2020-05-21 07:21:48 -0500

    feat: use local keystore and key
    Signed-off-by: meows <b5c6@protonmail.com>

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,22 @@
 require('chai/register-should');
-const HDWalletProvider = require('truffle-hdwallet-provider');
+const path = require("path");
+const PrivateKeyProvider = require('truffle-privatekey-provider');
 const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+const keyth = require('keythereum');
+
+const keyPubAddress = '7d38c531e7544625a4ea96437d7fe5c2918f3307';
+const keyFilePath = '/home/ia/dev/bakaoh/WrappedETC/keystore/key';
+const keyFileDir = '/home/ia/dev/bakaoh/WrappedETC/';
+
+const keyobj = keyth.importFromFile(keyPubAddress, keyFileDir);
+const privateKey = keyth.recover('', keyobj);
+const privateKeyHex = privateKey.toString('hex');
+
+console.log('private key', privateKeyHex);
+
+// const privateKey = fs.readFileSync("").toString().trim();
+// const privateKey = "0x7d38c531e7544625a4ea96437d7fe5c2918f3307";
+const provider = new PrivateKeyProvider(privateKeyHex, "http://localhost:7545");
 
 module.exports = {
 
@@ -10,18 +25,19 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
+      gas: 6350000,
       port: 7545,
       network_id: "*",
-    },
-
-    kotti: {
-      provider: () => new HDWalletProvider(mnemonic, "https://www.ethercluster.com/kotti"),
-      network_id: 6,
-      gasPrice: 2000000000,
-      confirmations: 2,
-      timeoutBlocks: 200,
-      skipDryRun: true
     }
+
+    // kotti: {
+    //   provider: () => new HDWalletProvider(mnemonic, "https://www.ethercluster.com/kotti"),
+    //   network_id: 6,
+    //   gasPrice: 2000000000,
+    //   confirmations: 2,
+    //   timeoutBlocks: 200,
+    //   skipDryRun: true
+    // }
   },
 
   // Configure your compilers
@@ -31,8 +47,8 @@ module.exports = {
        optimizer: {
          enabled: false,
          runs: 200
-       },
-       evmVersion: "byzantium"
+       } // ,
+       // evmVersion: "byzantium"
       }
     }
   }
